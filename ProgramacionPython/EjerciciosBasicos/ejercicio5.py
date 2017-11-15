8e3c529fc8f93ebc30484a84f127c223780b58e9
Jesús Sánchez De Castro <yussdc@gmail.com>
2017-11-15 19:45:24 +0100

    ejercicio 5 corregido

--- ProgramacionPython/EjerciciosBasicos/ejercicio5.py
+++ ProgramacionPython/EjerciciosBasicos/ejercicio5.py
@@ -1,17 +1,28 @@
 # Jesús Sánchez de Castro, Máster de ciencia de datos UGR 15/11/2017
 # Ejercicio 5:
-# Escribe una función num_vocales(palabra) que devuelva el número de vocales que aparece
-# en la palabra.
+# Escribe una función vocales(palabra) que devuelva las vocales que aparecen en la palabra.
 
-def num_vocales (palabra):
+
+def vocales (palabra):
 
     vocales = ['a','e','i','o','u']
-    contador = 0
+    vocales_en_palabra = []
     for char in palabra:
         if char.lower() in vocales:
-            contador+=1
+            vocales_en_palabra.append(char.lower())
 
-    return contador
+    return vocales_en_palabra
 
+def eliminar_repetidos (lista):
+
+    resultado = []
+    for i in lista:
+        if i not in resultado:
+            resultado.append(i)
+    return resultado
+
+
+palabra = "Murcielago"
+print("La palabra",palabra,"tiene las siguientes vocales ",eliminar_repetidos(vocales(palabra)))
 palabra = "Abracadabra"
-print("La palabra",palabra,"tiene",num_vocales(palabra),"vocales.")
+print("La palabra",palabra,"tiene las siguientes vocales ",eliminar_repetidos(vocales(palabra)))
