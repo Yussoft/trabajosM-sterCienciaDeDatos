cd913e79da604014764abce5ccd1afae35c3dee7
Jesús Sánchez De Castro <yussdc@gmail.com>
2017-11-15 17:21:35 +0100

    Ejercicio 5 terminado

--- ProgramacionPython/EjerciciosBasicos/ejercicio5.py
+++ ProgramacionPython/EjerciciosBasicos/ejercicio5.py
@@ -2,3 +2,16 @@
 # Ejercicio 5:
 # Escribe una función num_vocales(palabra) que devuelva el número de vocales que aparece
 # en la palabra.
+
+def num_vocales (palabra):
+
+    vocales = ['a','e','i','o','u']
+    contador = 0
+    for char in palabra:
+        if char.lower() in vocales:
+            contador+=1
+
+    return contador
+
+palabra = "Abracadabra"
+print("La palabra",palabra,"tiene",num_vocales(palabra),"vocales.")
