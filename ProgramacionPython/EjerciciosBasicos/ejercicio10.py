085f69de74ea2e7f174bbfed5cf25b7742a31b08
Jesús Sánchez De Castro <yussdc@gmail.com>
2017-11-16 17:09:36 +0100

    Ejercicios terminados

---
+++ ProgramacionPython/EjerciciosBasicos/ejercicio10.py
@@ -0,0 +1,4 @@
+# Jesús Sánchez de Castro, Máster de ciencia de datos UGR 15/11/2017
+# Ejercicio 10;
+# Escribe una función palindromo(frase) que determine si frase es un palíndromo. Es decir,
+# que se lea igual de izquierda a derecha que de derecha a izquierda (sin considerar espacios).
